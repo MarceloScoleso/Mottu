c030de927ec259d808d16a8efedbce96a8f4ad0b
MarceloScoleso <marcelo.scolesojr@gmail.com>
2025-05-14 09:49:03 -0300

    Ajustes moto

--- Models/Moto.cs
+++ Models/Moto.cs
@@ -5,18 +5,30 @@ namespace Mottu.Models
     public class Moto
     {
         [Key]
-        public int Id_Moto { get; set; }  // Chave primária
+        public int Id_Moto { get; set; }
+
+        [Required]
         public string Placa { get; set; }
+
+        [Required]
         public string Modelo { get; set; }
+
+        [Required]
+        public int Ano { get; set; }
+
+        [Required]
         public string Status_Atual { get; set; }
 
+        // Construtor vazio necessário para o Entity Framework
         public Moto() { }
 
-        public Moto(int idMoto, string placa, string modelo, string statusAtual)
+        // Construtor com todos os campos usados
+        public Moto(int idMoto, string placa, string modelo, int ano, string statusAtual)
         {
             Id_Moto = idMoto;
             Placa = placa;
             Modelo = modelo;
+            Ano = ano;
             Status_Atual = statusAtual;
         }
     }
